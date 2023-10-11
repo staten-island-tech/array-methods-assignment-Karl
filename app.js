439531e0f9ef54cca74f21f7850c5916f9a3cbb0
phanther1 <144461089+phanther1@users.noreply.github.com>
2023-10-11 10:13:50 -0400

    Object inputing, working on array and filter

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
-const monky = {
-    firstName: "Monky",
-    lastName: "Monkyson",
-    bananaCount: 25,
-    vibeStatus: true,
-    quote: "ooga booga",
-    addBanana: function () {
-        this.bananaCount++;
+const animals = [
+    {
+        speciesName: "Monkey",
+        scientificName: "Bleb bleb",
+        location: "Land",
+        lifeSpan: 25,
+        ancient: true,
+        status: "endangered",
     },
-    fullName: function () {
-        return `${this.firstName} ${this.lastName}`;
+    {
+        speciesName: "Blue Whale",
+        scientificName: "bleb bleb",
+        location: "Ocean",
+        lifeSpan: 85,
+        ancient: true,
+        status: "endangered",
     },
-};
+    {
+        speciesName: "Seal",
+        scientificName: "bleb bleb",
+        location: "Ocean",
+        lifeSpan: 25,
+        ancient: true,
+        status: "Least Concern",
+    },
+    {
+        speciesName: "Blue Jay",
+        scientificName: "bleb bleb",
+        location: "Forest",
+        lifeSpan: 25,
+        ancient: true,
+        status: "Least Concern",
+    },
+    
+]
 
-arr 
+animals.forEach((el)=> console.log(el.speciesName));
+
+const test = animals.filter((animals)=> "Ocean");
